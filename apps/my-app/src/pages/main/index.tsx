fab933f704f1fb3a17e1a56e396a9d0a491e4080
seonjeong <theblessedcity@hanmail.net>
2023-05-28 00:50:53 +0900

    feat: add hover func in main

--- apps/my-app/src/pages/main/index.tsx
+++ apps/my-app/src/pages/main/index.tsx
@@ -38,9 +38,11 @@ const Main = () => {
           left={
             <>
               <ButtonIcon icon="arrow-right" onClick={() => {}} />
-              <Button shape="basic" variant="secondary" onClick={() => {}}>
-                닉네임 괴도님! 지금까지 00000원 루팡 하셨어요!
-              </Button>
+              <div className="hover-box">
+                <Button shape="basic" variant="secondary" onClick={() => {}}>
+                  닉네임 괴도님! 지금까지 00000원 루팡 하셨어요!
+                </Button>
+              </div>
             </>
           }
         />
@@ -48,56 +50,58 @@ const Main = () => {
           left={
             <>
               <ButtonIcon icon="arrow-right" onClick={() => {}} />
-              <Button
-                shape="basic"
-                variant={page === 'explore' ? 'primary' : 'secondary'}
-                onClick={() => {
-                  if (page === 'explore') return;
-                  setPage('explore');
-                }}
-              >
-                검색하기
-              </Button>
-              <Button
-                shape="basic"
-                variant={page === 'gamea' ? 'primary' : 'secondary'}
-                onClick={() => {
-                  if (page === 'gamea') return;
-                  setPage('gamea');
-                }}
-              >
-                게임1
-              </Button>
-              <Button
-                shape="basic"
-                variant={page === 'gameb' ? 'primary' : 'secondary'}
-                onClick={() => {
-                  if (page === 'gameb') return;
-                  setPage('gameb');
-                }}
-              >
-                게임2
-              </Button>
-              <Button
-                shape="basic"
-                variant={page === 'recommandmenu' ? 'primary' : 'secondary'}
-                onClick={() => {
-                  if (page === 'recommandmenu') return;
-                  setPage('recommandmenu');
-                }}
-              >
-                점심메뉴추천
-              </Button>
-              <Button
-                shape="basic"
-                variant={page === 'travelinfo' ? 'primary' : 'secondary'}
-                onClick={() => {
-                  if (page === 'travelinfo') return;
-                  setPage('travelinfo');
-                }}
-              >
-                여행정보수집
-              </Button>
+              <div className="hover-box">
+                <Button
+                  shape="basic"
+                  variant={page === 'explore' ? 'primary' : 'secondary'}
+                  onClick={() => {
+                    if (page === 'explore') return;
+                    setPage('explore');
+                  }}
+                >
+                  검색하기
+                </Button>
+                <Button
+                  shape="basic"
+                  variant={page === 'gamea' ? 'primary' : 'secondary'}
+                  onClick={() => {
+                    if (page === 'gamea') return;
+                    setPage('gamea');
+                  }}
+                >
+                  게임1
+                </Button>
+                <Button
+                  shape="basic"
+                  variant={page === 'gameb' ? 'primary' : 'secondary'}
+                  onClick={() => {
+                    if (page === 'gameb') return;
+                    setPage('gameb');
+                  }}
+                >
+                  게임2
+                </Button>
+                <Button
+                  shape="basic"
+                  variant={page === 'recommandmenu' ? 'primary' : 'secondary'}
+                  onClick={() => {
+                    if (page === 'recommandmenu') return;
+                    setPage('recommandmenu');
+                  }}
+                >
+                  점심메뉴추천
+                </Button>
+                <Button
+                  shape="basic"
+                  variant={page === 'travelinfo' ? 'primary' : 'secondary'}
+                  onClick={() => {
+                    if (page === 'travelinfo') return;
+                    setPage('travelinfo');
+                  }}
+                >
+                  여행정보수집
+                </Button>
+              </div>
             </>
           }
           right={<Switch checked={true} onChange={() => {}} />}
@@ -106,10 +110,12 @@ const Main = () => {
         <Screen.Item
           right={
             <>
-              <Button shape="basic" variant="primary">
-                배경설정하기
-              </Button>
               <ButtonIcon icon="arrow-left" onClick={() => {}} />
+              <div className="hover-box">
+                <Button shape="basic" variant="primary">
+                  배경설정하기
+                </Button>
+              </div>
             </>
           }
         />
