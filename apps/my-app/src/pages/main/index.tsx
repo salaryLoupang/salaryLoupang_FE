57b8701d8055d59e43ebc3ae2ef98a49878c8084
seonjeong <theblessedcity@hanmail.net>
2023-05-27 16:03:08 +0900

    feat: change screen chidlren by button

--- apps/my-app/src/pages/main/index.tsx
+++ apps/my-app/src/pages/main/index.tsx
@@ -9,10 +9,28 @@ import { Screen } from 'components/Screen';
 
 import Explore from './Explore';
 import GameA from './GameA';
+import GameB from './GameB';
 import RecommandMenu from './RecommandMenu';
 import TravelInfo from './TravelInfo';
 
 const Main = () => {
+  const [page, setPage] = React.useState('explore');
+
+  const renderScreen = () => {
+    switch (page) {
+      case 'explore':
+        return <Explore />;
+      case 'gamea':
+        return <GameA />;
+      case 'gameb':
+        return <GameB />;
+      case 'recommandmenu':
+        return <RecommandMenu />;
+      case 'travelinfo':
+        return <TravelInfo />;
+    }
+  };
+
   return (
     <div className="main-page">
       <Screen>
@@ -30,28 +48,61 @@ const Main = () => {
           left={
             <>
               <ButtonIcon icon="arrow-right" onClick={() => {}} />
-              <Button shape="basic" variant="primary">
+              <Button
+                shape="basic"
+                variant={page === 'explore' ? 'primary' : 'secondary'}
+                onClick={() => {
+                  if (page === 'explore') return;
+                  setPage('explore');
+                }}
+              >
                 검색하기
               </Button>
-              <Button shape="basic" variant="secondary">
+              <Button
+                shape="basic"
+                variant={page === 'gamea' ? 'primary' : 'secondary'}
+                onClick={() => {
+                  if (page === 'gamea') return;
+                  setPage('gamea');
+                }}
+              >
                 게임1
               </Button>
-              <Button shape="basic" variant="secondary">
+              <Button
+                shape="basic"
+                variant={page === 'gameb' ? 'primary' : 'secondary'}
+                onClick={() => {
+                  if (page === 'gameb') return;
+                  setPage('gameb');
+                }}
+              >
                 게임2
               </Button>
-              <Button shape="basic" variant="secondary">
+              <Button
+                shape="basic"
+                variant={page === 'recommandmenu' ? 'primary' : 'secondary'}
+                onClick={() => {
+                  if (page === 'recommandmenu') return;
+                  setPage('recommandmenu');
+                }}
+              >
                 점심메뉴추천
               </Button>
-              <Button shape="basic" variant="secondary">
+              <Button
+                shape="basic"
+                variant={page === 'travelinfo' ? 'primary' : 'secondary'}
+                onClick={() => {
+                  if (page === 'travelinfo') return;
+                  setPage('travelinfo');
+                }}
+              >
                 여행정보수집
               </Button>
             </>
           }
           right={<Switch checked={true} onChange={() => {}} />}
         />
-        <Screen.Box>
-          <RecommandMenu />
-        </Screen.Box>
+        <Screen.Box>{renderScreen()}</Screen.Box>
         <Screen.Item
           right={
             <>
