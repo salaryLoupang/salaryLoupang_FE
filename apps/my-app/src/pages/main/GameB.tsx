25adaa8553b658cbddcdc2a54e5ea616efc053fd
seonjeong <theblessedcity@hanmail.net>
2023-05-28 16:53:03 +0900

    feat: change gmaeB

--- apps/my-app/src/pages/main/GameB.tsx
+++ apps/my-app/src/pages/main/GameB.tsx
@@ -5,7 +5,7 @@ const GameB = () => {
     <iframe
       width="100%"
       height="100%"
-      src="https://woojuu.tistory.com/"
+      src="https://archive.org/details/msdos_Prehistorik_2_1993"
     ></iframe>
   );
 };
